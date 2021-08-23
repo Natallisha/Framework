f99c3eaf786c75982750171dba03fa33e609841c
Natallia Ivanova <ivanova.natallisha@gmail.com>
2021-08-23 12:55:26 +0300

    add gitignore

---
+++ src/test/java/com/google/cloud/page/AbstractPage.java
@@ -0,0 +1,15 @@
+package com.google.cloud.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class AbstractPage {
+
+    protected WebDriver driver;
+
+    protected final int WAIT_TIMEOUT_SECONDS = 20;
+
+    protected AbstractPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
